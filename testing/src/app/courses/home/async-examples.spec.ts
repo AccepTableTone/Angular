d778dc8b367f2d45ab2012f14bb049379eb84f44
david <david@davidmckinnon.ca>
2020-02-07 10:34:00 -0500

    testing - flushMicrotasks

--- testing/src/app/courses/home/async-examples.spec.ts
+++ testing/src/app/courses/home/async-examples.spec.ts
@@ -1,8 +1,44 @@
-import { fakeAsync, tick, flush } from "@angular/core/testing";
+import { fakeAsync, tick, flush, flushMicrotasks } from "@angular/core/testing";
 
 /** default test timeout is 5 seconds */
 
 fdescribe("Async testing example", () => {
+  it("Asynchronous test - task and microtask - setTimeout and Promise", fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+    /** flush promise - expecting 10 */
+    flushMicrotasks();
+    expect(counter).toBe(10);
+
+    flush();
+    expect(counter).toBe(11);
+  }));
+
+  it("Asynchronous test - Promise", fakeAsync(() => {
+    let test = false;
+
+    Promise.resolve()
+      .then(() => {
+        return Promise.resolve();
+      })
+      .then(() => {
+        test = true;
+      });
+
+    flushMicrotasks();
+
+    expect(test).toBeTruthy();
+  }));
+
   /** 'zones' are chunks of code to be executed post async  */
   it("Asynchronous test example with angular fake async utility", fakeAsync(() => {
     let test = false;
